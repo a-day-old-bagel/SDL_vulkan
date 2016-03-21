7134ced916811ee64a184e122f18126db2a7138e
David McFarland <corngood@gmail.com>
2016-03-21 16:33:04 -0300

    remove extern

--- include/SDL_vulkan.h
+++ include/SDL_vulkan.h
@@ -5,6 +5,6 @@
 #include <vulkan/vulkan.h>
 
 SDL_bool SDL_GetVulkanInstanceExtensions(unsigned* count, const char** names);
-extern SDL_bool SDL_CreateVulkanSurface(SDL_Window* window, VkInstance instance, VkSurfaceKHR* surface);
+SDL_bool SDL_CreateVulkanSurface(SDL_Window* window, VkInstance instance, VkSurfaceKHR* surface);
 
 #endif /* _SDL_vulkan_h */
